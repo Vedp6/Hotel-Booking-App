9e64f634607c44482c635ca7a2dfd287e9646b3b
Vedp6 <vedantpatil15208@gmail.com>
2026-03-30 11:09:27 +0530

    new

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeatureDestination from '../components/FeatureDestination'
+import ExclusiveOffer from '../components/ExclusiveOffer'
+import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <>
+      <Hero/>
+      <FeatureDestination/>
+      <ExclusiveOffer/>
+      <Testimonial/>
+      <NewsLetter/>
+      
+    </>
+  )
+}
+
+export default Home
